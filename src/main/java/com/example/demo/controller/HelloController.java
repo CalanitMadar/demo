d210ab0a4037baa9d01cb3c4b2145b97034c7793
Calanit Madar <110593229+CalanitMadar@users.noreply.github.com>
2023-08-30 12:36:58 +0300

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("/test")
+    @GetMapping("/index.html")
     public String index() {
         return "index";
     }
